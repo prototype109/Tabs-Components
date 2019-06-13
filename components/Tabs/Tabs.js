e5df2fc718986c47c315f8b2bc9899213e8ef4de
Brandon Nichols <brandonaronnichols@gamil.com>
2019-06-12 23:44:20 -0500

    Added querySelector for data-tab when the link is passed in so can manipulate specific tab

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,7 +8,7 @@ class TabLink {
     this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    this.itemElement = documnet.querySelector(`[data-tab="${this.data}"]`);
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
