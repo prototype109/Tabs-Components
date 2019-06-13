baded75ac9a29ffa8ccf02ac4566e94ab8c3ab79
Brandon Nichols <brandonaronnichols@gamil.com>
2019-06-13 00:21:12 -0500

    Added event listener for this.element which is the tab link being passed in that on click calls the select method which changes the color and background of the tab

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -25,27 +25,29 @@ class TabLink {
     this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement = documnet.querySelector(`[data-tab="${this.data}"]`);
+    this.itemElement = document.querySelector(`[data-tab="${this.data}"]`);
     
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', this.select.bind(this));
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
+    links.forEach(link => {
+      link.classList.remove('tabs-link-selected');
+    })
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.toggle('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
   }
 }
 
@@ -61,4 +63,4 @@ class TabLink {
 */
 
 let links = document.querySelectorAll('.tabs-link');
-links.forEach(link => new TabItem(link));
+links.forEach(link => new TabLink(link));
